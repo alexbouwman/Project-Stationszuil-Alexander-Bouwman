1b388508c21e7d11d992460cf0a3d17307e99f1d
alexbouwman <alexander.bouwman@student.hu.nl>
2022-09-23 11:45:01 +0200

    Update of all files.

---
+++ Zuil/Module 1 Zuil.py
@@ -0,0 +1,36 @@
+from datetime import datetime
+from random import randint
+
+vandaag = datetime.today()
+
+
+def random_station():
+    stations_lijst = open('stations.txt', 'r')
+    stations = stations_lijst.readlines()
+    station = stations[randint(0, 2)]
+    stations_lijst.close()
+    return station
+
+
+def mening():
+    berichten = open('berichten.csv', 'a')
+    naam = input("Voer uw naam in: ")
+    tijd = vandaag.strftime('%d/%m/%Y %H:%M:%S')
+    if len(naam) <= 0:
+        naam = "Anoniem"
+    bericht = input("Geef uw mening over dit station: ")
+    if len(bericht) > 140:
+        print("Bericht is te lang. Probeer opnieuw.")
+    if naam == 'clear':
+        clear()
+    elif len(bericht) < 140:
+        berichten.write(f'{naam};{tijd};{bericht};{random_station()}\n')
+        print("Bericht opgeslagen, dank voor uw mening!")
+
+def clear():
+    berichten_clear = open('berichten.csv', 'w')
+    berichten_clear.write('Naam;Datum/Tijd;Bericht;Station\n')
+
+
+while True:
+    mening()
