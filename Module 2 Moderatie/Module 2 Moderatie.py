1d6e063aa6721143804e62f3a1689136969ffc88
alexbouwman <alexander.bouwman@student.hu.nl>
2022-10-04 11:47:17 +0200

    Update of Module 2 Moderatie

--- Module 2 Moderatie/Module 2 Moderatie.py
+++ Module 2 Moderatie/Module 2 Moderatie.py
@@ -1,31 +1,34 @@
 from datetime import datetime
 
+
 def beoordeling():
+    beoordeeld = []
     for x in onbeoordeeld:
         beoordeeld.append(x.strip().split(';'))
     for x in beoordeeld:
-        print(f'{x[0]}: {x[2]}')
+        print(f'{x[0]} {x[1]} {x[3]}: {x[2]}')
         beoordeling = input('Is dit bericht toegestaan? Ja/Nee: ').lower()
         if beoordeling == 'ja':
             x.append('Goedgekeurd')
             print(beoordeeld)
         elif beoordeling == 'nee':
             x.append('Afgekeurd')
+        x.append(moderatornaam)
+        x.append(moderatortijd)
+        x.append(moderatoremail)
     return beoordeeld
 
 
 with open('../berichten.csv') as meningen:
     onbeoordeeld = meningen.readlines()
-    beoordeeld = []
     onbeoordeeld.remove('Naam;Datum/Tijd;Bericht;Station\n')
-
 
 vandaag = datetime.today()
 moderatornaam = input('Moderator naam: ')
 moderatortijd = vandaag.strftime('%d/%m/%Y %H:%M:%S')
 moderatoremail = input('Moderator E-mail: ')
 
-
-x.append(moderatornaam)
-x.append(moderatortijd)
-x.append(moderatoremail)
+meningen_toevoegen = open('../berichten.csv', 'w')
+meningen_toevoegen.write('Naam;Datum/Tijd;Bericht;Station;Status;Moderator;Datum/Tijd-MOD;Email-MOD\n')
+for x in beoordeling():
+    meningen_toevoegen.write(f'{x[0]};{x[1]};{x[2]};{x[3]};{x[4]};{x[5]};{x[6]};{x[7]}\n')
